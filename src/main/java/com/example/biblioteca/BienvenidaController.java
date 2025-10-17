426de4c42b86fdd80e8051af0033a57191a19e8d
jesusdd36 <jesusdd36@educastur.es>
2025-10-17 19:51:53 +0200

    cambio en la release

--- src/main/java/com/example/biblioteca/BienvenidaController.java
+++ src/main/java/com/example/biblioteca/BienvenidaController.java
@@ -5,4 +5,6 @@ public class BienvenidaController {
     static void main() {
         System.out.println("Bienvenido Monte Naranco");
     }
+
+    String release = ("Mensaje nuevo para la release");
 }
