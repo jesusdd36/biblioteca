7ed8a029dcafb7e984118619e7eb61c89fa28877
jesusdd36 <jesusdd36@educastur.es>
2025-10-31 20:17:58 +0100

    Realizado la hotfix

---
+++ src/main/java/com/example/biblioteca/controller/LibroController.java
@@ -0,0 +1,33 @@
+package com.example.biblioteca.controller;
+
+import com.example.biblioteca.entity.Libro;
+import com.example.biblioteca.service.LibroServideImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+
+
+@RestController
+@RequestMapping(path = "/api")
+public class LibroController {
+
+    @Autowired
+    private LibroServideImpl libroServiceImpl; // Se inyecta la dependencia
+
+
+    @GetMapping
+    public List<Libro> obtenerTodos() { return this.libroServiceImpl.getLibros(); }
+
+    @GetMapping("/{id}")
+    public Optional<Libro> getLibro(@PathVariable Long id) { return this.libroServiceImpl.getLibro(id); }
+
+    @PostMapping
+    public void guardarOActualizarLibro(@RequestBody Libro libro) { this.libroServiceImpl.guardarOActualizarLibro(libro); }
+
+    @DeleteMapping("/libroid")
+    public void eliminarLibro(@PathVariable("libroId") Long libroId) { this.libroServiceImpl.eliminarLibro(libroId); }
+
+
+}
